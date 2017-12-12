f30fe08076af90307f58819e994a012c627b5c23
Nicholas Perez <nperez9@mail.greenriver.edu>
2017-12-12 08:42:18 -0800

    Added validation

--- js/js.js
+++ js/js.js
@@ -66,6 +66,40 @@ const app = new Vue({
                     this.isPrice = false;
                     this.isConfirm = false;
                 }
+            },
+
+            check: function () {
+                var fname = $('#fname');
+                var lname = $('#lname');
+                var email = $('#email');
+                var tel = $('#tel');
+                var event = $('#event');
+                var people = $('#people');
+
+                var errors = 0;
+                $('.inputError').html('');
+
+                var inputs = [fname, lname, email, tel, event, people];
+                var errorMsg = [
+                    "Enter a valid first name",
+                    "Enter a valid last name",
+                    "Enter a valid email",
+                    "Enter a valid phone number",
+                    "Enter a valid event",
+                    "Enter a valid person count"
+                ];
+
+                for(var i =0;i<inputs.length;i++) {
+                    if (inputs[i].val() === '') {
+                        errors++;
+                        inputs[i].before('<span class="inputError">' + errorMsg[i] + '</span>');
+                    }
+                }
+
+                if(errors === 0)
+                {
+                    app.confirm();
+                }
             }
         }
 });
